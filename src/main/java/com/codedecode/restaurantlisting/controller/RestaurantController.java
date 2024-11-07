7ffb85297e547fdc4405d876d7261fdd270623f7
msedky <m.hosamsedky@hotmail.com>
2024-11-07 16:18:47 +0200

    just add some empty lines in controller class to test jenkins deployment when commit and push

--- src/main/java/com/codedecode/restaurantlisting/controller/RestaurantController.java
+++ src/main/java/com/codedecode/restaurantlisting/controller/RestaurantController.java
@@ -23,6 +23,8 @@ public class RestaurantController {
 
 	@Autowired
 	RestaurantService restaurantService;
+	
+	
 
 	@GetMapping("/fetchAllRestaurants")
 	public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants() {
